e8f40883932bc1422c5f4343c25b49b41ced3f33
Arthur Giroux <arthurgiroux@gmail.com>
2018-01-03 22:49:13 +0100

    Functional power / tare buttons. Scaling/displaying the weight to grams.

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,34 +1,83 @@
 #include "HX711.h"
-#include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
+#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
 
 Adafruit_7segment matrix = Adafruit_7segment();
 
-#define calibration_factor -7050.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
+// This value was found by calling set_scale() and tare().
+// putting an object of known weight (101g) and putting it on the scale.
+// averaging the results of scale.get_units(10);
+// you then take this average and divide it by the weight of your object.
+const float calibrationFactor = 21780.0f / 101.0f;
 
-#define DOUT  3
-#define CLK  2
+const int scaleDOUTPin = 3;
+const int scaleCLKPin = 2;
 
-HX711 scale(DOUT, CLK);
+const int powerButtonPin = 8;
+const int tareButtonPin = 9;
+bool displayIsOn = true;
+bool waitForPowerButtonReset = false;
+
+bool waitForTareButtonReset = false;
+
+HX711 scale;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("HX711 scale demo");
 
-  scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
-  scale.tare();  //Assuming there is no weight on the scale at start up, reset the scale to 0
+  // Initialize the scale
+  scale.begin(scaleDOUTPin, scaleCLKPin);
+  scale.set_scale(calibrationFactor);
+  scale.tare();
 
-  Serial.println("Readings:");
+  // Initialize the screen
   matrix.begin(0x70);
+  matrix.clear();
+  matrix.writeDisplay();
 
+  // Initialize the buttons
+  pinMode(powerButtonPin, INPUT);
+  pinMode(tareButtonPin, INPUT);
 }
 
 void loop() {
-  Serial.print("Reading: ");
-  Serial.print(scale.get_units(), 1); //scale.get_units() returns a float
-  matrix.print(abs(scale.get_units()));
-  matrix.writeDisplay();
-  Serial.print(" lbs"); //You can change this to kg but you'll need to refactor the calibration_factor
-  Serial.println();
+
+  // if the power button is pressed then we switch on/off the screen
+  int powerButtonIsPressed = digitalRead(powerButtonPin);
+  if (powerButtonIsPressed) {
+    // we only do something at the rising edge
+    if (!waitForPowerButtonReset) {
+      Serial.println("power button pressed");
+      scale.tare();
+      displayIsOn = !displayIsOn;
+      waitForPowerButtonReset = true;
+      matrix.clear();
+      matrix.writeDisplay();
+    }
+  }
+  else {
+    waitForPowerButtonReset = false;
+  }
+
+  int tareButtonIsPressed = digitalRead(tareButtonPin);
+  if (tareButtonIsPressed) {
+    // we only do something at the rising edge
+    if (!waitForTareButtonReset) {
+      Serial.println("tare button pressed");
+      waitForTareButtonReset = true;
+      scale.tare();
+    }
+  }
+  else {
+    waitForTareButtonReset = false;
+  }
+
+  // we retrieve the readings of the scale and display them
+  if (displayIsOn) {
+    int value = scale.get_units();
+    matrix.print(value);
+    Serial.println(value);
+    matrix.writeDisplay();
+  }
 }
